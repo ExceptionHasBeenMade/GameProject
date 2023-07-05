c4958e25b47d671c33cb45f47ee184f61439b033
Ktos <kornelkozierkiewicz@gmail.com>
2023-07-05 09:52:45 +0200

    Other type of magazine json created

--- GameProject/Helpers/magazineCreator.py
+++ GameProject/Helpers/magazineCreator.py
@@ -16,10 +16,15 @@ connector = open(pathOfHarbors)
 importedHarbors = json.load(connector)
 connector.close()
 
+importedMagazines = {"magazine" : {}}
+
 for harbor in importedHarbors["alls"]:
+    importedMagazines["magazine"].update({harbor : {}})
     for category in importedResources["resources"]:
+        # importedMagazines["magazine"][harbor].update({category : {}})
         for item in importedResources["resources"][category]:
             importedMagazines["magazine"][harbor].update({item : 0})
+            # importedMagazines["magazine"][harbor][category].update({item : 0})
 
 connector = open(pathOfMagazines, "w", encoding="utf-8")
 json.dump(importedMagazines, connector)
