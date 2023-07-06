7e48d013faa66b15a50feb850fe4520837accc49
Ktos <kornelkozierkiewicz@gmail.com>
2023-07-06 22:59:04 +0200

    continue working on prices.json

--- GameProject/Helpers/priceCreator.py
+++ GameProject/Helpers/priceCreator.py
@@ -11,14 +11,32 @@ connector = open(pathOfPrices)
 importedPrices = json.load(connector)
 connector.close()
 
-importedPrices = {"price" : {}}
+# importedPrices = {"price" : {}}
+i = 0 
 
 for category in importedResources["resources"]:
     for item in importedResources["resources"][category]:
-        price = input(f"Pass prices of {item}\n")
-        unit = input(f"Pass unit of {item}\n")
-        importedPrices["price"].update({item : [price, unit]})
+        if(i == 0):
+            price = input(f"Pass prices of {item}\n")
+            unit = input(f"Pass unit of {item}\n")
+            energyValue = input(f"Pass energy value of {item}\n") # In kWh/kg
+            importedPrices["price"].update({item : [float(price), unit, float(energyValue)]})
+        elif(i == 1):
+            price = input(f"Pass price of {item}\n")
+            unit = input(f"Pass unit of {item}\n") # pc - one piece
+            inaccessible = input(f"Pass chances (in %) of being inaccessible of {item}\n")
+            importedPrices["price"].update({item : [float(price), unit, float(inaccessible)/100]})
+        elif(i == 2):
+            pass
+        else:
+            continue
+    i+=1
+    connector = open(pathOfPrices, "w", encoding="utf-8")
+    json.dump(importedPrices, connector)
+    connector.close()
 
-connector = open(pathOfPrices, "w", encoding="utf-8")
-json.dump(importedPrices, connector)
-connector.close()
+
+
+
+
+
